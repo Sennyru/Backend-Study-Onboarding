3bffeed41e25c66e7a1681180d0889f955fa3fc2
Sennyru <duwnsgur11012@gmail.com>
2025-09-24 21:54:12 +0900

    fix: 중복 이메일 생성 방지
    - 사용자 DB에는 중복 이메일이 존재할 수 없음
    - DB에 존재하는 이메일과 같은 이메일로 요청이 들어왔을 경우, 기존에는 응답이 오지 않았음
    - 이제 400 Bad Request 응답

--- src/main/java/com/sennyru/onboarding/repository/MemberRepository.java
+++ src/main/java/com/sennyru/onboarding/repository/MemberRepository.java
@@ -4,4 +4,5 @@ import com.sennyru.onboarding.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
 }
