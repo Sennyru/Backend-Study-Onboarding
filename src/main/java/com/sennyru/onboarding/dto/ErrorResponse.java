3d4c344e127d5dfc344167c4fa69f880be053bc5
Sennyru <duwnsgur11012@gmail.com>
2025-09-30 20:21:06 +0900

    [#1]; refactor: DTO를 record 클래스로 변경 - 클래스 코드를 깔끔하게 하기 위하여 DTO들을 record 클래스로 변경함

--- src/main/java/com/sennyru/onboarding/dto/ErrorResponse.java
+++ src/main/java/com/sennyru/onboarding/dto/ErrorResponse.java
@@ -1,25 +1,15 @@
 package com.sennyru.onboarding.dto;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 
-@Getter
-public class ErrorResponse {
-
-    private final LocalDateTime time = LocalDateTime.now();
-    private final String status;
-    private final String message;
-    private final String requestURI;
-
-    private ErrorResponse(HttpStatus status, String message, String requestURI) {
-        this.status = status.name();
-        this.message = message;
-        this.requestURI = requestURI;
-    }
-
+public record ErrorResponse(
+    LocalDateTime time,
+    String status,
+    String message,
+    String requestURI
+) {
     public static ErrorResponse of(HttpStatus status, String message, String requestURI) {
-        return new ErrorResponse(status, message, requestURI);
+        return new ErrorResponse(LocalDateTime.now(), status.name(), message, requestURI);
     }
 }
--- src/main/java/com/sennyru/onboarding/dto/SignupRequestDto.java
+++ src/main/java/com/sennyru/onboarding/dto/SignupRequestDto.java
@@ -2,26 +2,18 @@ package com.sennyru.onboarding.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
 
-@Getter
-public class SignupRequestDto {
+public record SignupRequestDto(
     @NotBlank
     @Email
-    private final String email;
+    String email,
 
     @NotBlank
-    private final String password;
+    String password,
 
     @NotBlank
-    private final String username;
-
-    private SignupRequestDto(String email, String password, String username) {
-        this.email = email;
-        this.password = password;
-        this.username = username;
-    }
-
+    String username
+) {
     public static SignupRequestDto of(String email, String password, String username) {
         return new SignupRequestDto(email, password, username);
     }
